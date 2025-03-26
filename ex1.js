3bfaa6323f3e0df9394641447b151790b53055c8
ErebosXYZ <robertmaynou@gmail.com>
2025-03-26 18:38:26 +0100

    done

--- ex1.js
+++ ex1.js
@@ -9,22 +9,36 @@
  * Cambia el título de la página a rojo usando un estilo en línea
  */
 
+document.querySelector("#title").style.color = "red";
+
 /**
  * Cambia el tamaño del título de la página a 48px usando un estilo en línea
  */
+
+document.querySelector("#title").style.fontSize = "48px";
 
 /**
  * Muestra por console.log el texto de la caja 2
  */
 
+console.log(document.querySelector("#caja-2").textContent);
+
 /**
  * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
  */
+
+document.querySelector(".intro").style.border = "green 10px solid";
 
 /**
  * Elimina la clase "estilo-3" de la última caja.
  * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
  */
+
+function removeClass() {
+    let removedClass = document.querySelector("#ultimo");
+    removedClass.classList.remove("estilo-3");
+}
+removeClass();
 
 /**
  * Añade la clase CSS "ocultar" al penúltimo párrafo. La última cita debería desaparecer. Compruebalo con el inspector de elementos. 
@@ -33,3 +47,6 @@
  * BONUS: Haz el ejercicio sin modificar el ex1.html
  
  */
+
+let penultimo = document.querySelector("p:nth-of-type(6)");
+penultimo.classList.add("ocultar");
